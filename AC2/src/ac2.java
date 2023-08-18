f8c36c4763a2d79d5d305f61bd17eaa9438abcd3
Esther Pessanha <eympessanha@gmail.com>
2023-08-18 11:18:12 -0300

    Update ac2.java

--- AC2/src/ac2.java
+++ AC2/src/ac2.java
@@ -3,73 +3,69 @@ import java.util.Scanner;
 public class ac2 {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        double num1;
-        double num2;
+        double result;
+        double n;
         String oper; 
         String msg = "A";
 
         while(msg.equals("A")){
-        System.out.println("Informe um valor double: ");
-        num1 = leitor.nextDouble(); 
-        System.out.println("Informe qual operação deseja fazer (soma, sub, div, mult): ");
-        oper = leitor.next();
-        System.out.println("Informe outro valor double: ");
-        num2 = leitor.nextDouble(); 
-        operacao(oper, num1, num2);
-        mensagem(msg);
-        msg = leitor.next();
-        while(msg.equals("B")){ 
-            System.out.println("Informe qual operação deseja fazer (soma, sub, div, mult): ");
-            oper = leitor.next();
-            System.out.println("Informe outro valor double: ");
-            num2 = leitor.nextDouble(); 
-            operacao(oper, num1, num2);
-            mensagem(msg);
-            msg = leitor.next();
-        }
+            System.out.println("Informe um valor double: ");
+            result = leitor.nextDouble(); 
+            msg = "B";
+                while(msg.equals("B")){
+                    System.out.println("Informe qual operação deseja fazer (soma, sub, div, mult): ");
+                    oper = leitor.next();
+                    System.out.println("Informe outro valor double: ");
+                    n = leitor.nextDouble(); 
+                    result = operacao(oper, result, n);
+                    System.out.println("Para recomeçar o cálculo, digite A");
+                    System.out.println("Para escolher outra operação, digite B");
+                    System.out.println("Para sair, digite X: ");
+                    msg = leitor.next();
+                }
         }
         System.out.println("Calculadora finalizada");
         leitor.close();
     }
 
-    public static double soma(double num1, double num2){
-        return num1+num2;
+    public static double soma(double result, double n){
+        return result+n;
     }
 
-    public static double sub(double num1, double num2){
-        return num1-num2; 
+    public static double sub(double result, double n){
+        return result-n;
     }
 
-    public static double div(double num1, double num2){
-        return num1/num2; 
+    public static double div(double result, double n){
+        return result/n;
     }
 
-    public static double mult(double num1, double num2){
-        return num1*num2;
+    public static double mult(double result, double n){
+        return result*n;
     }
-
-    public static void operacao(String oper,double num1, double num2){
+    
+    public static double operacao(String oper,double result, double n){
         switch(oper){
             case "soma":
-                System.out.println(soma(num1, num2));
+                result = soma(result, n);
+                System.out.println(result);
                 break;
             case "sub":
-                System.out.println(sub(num1, num2));
+                result = sub(result, n);
+                System.out.println(result);
                 break;
             case "div":
-                System.out.println(div(num1, num2));
+                result = div(result, n);
+                System.out.println(result);
                 break;
             case "mult":
-                System.out.println(mult(num1, num2));
+                result = mult(result, n);
+                System.out.println(result);
                 break;
             default:
                 System.out.println("Operação inexistente.");
+            
         }
-    }
-
-    public static void mensagem(String msg){
-        System.out.println("Para recomeçar o cálculo, digite A");
-        System.out.println("Para escolher outra operação, digite B");
-        System.out.println("Para sair, digite X: ");
+        return result;
     }
 }
